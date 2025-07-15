e0e6ec0a04200d3770d2676fcd87f6063e797e8f
earthdiary <127043563+earthdiary@users.noreply.github.com>
2025-07-15 15:14:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer-extra');
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
